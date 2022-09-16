8cd450b03f48daa23895ab045cb8ef83174ea750
Dmitrii Rudchenko <rudchenko.d.v@gmail.com>
2022-09-16 20:12:51 +0300

    final check5

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,23 +1,27 @@
-# import random
+import random
+import math
 
-# import sympy
 from brain_games.main_logic import all_games_logic
 
 rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+
+def is_prime(n):
+  for i in range(2,int(math.sqrt(n))+1):
+    if (n%i) == 0:
+      return False
+  return True
+
+
 def game_logic():
+    end_number = 50
+    number_f = random.randint(2, end_number)
 
-    number_f = 5
-    meaning = 'yes'
-
-    # end_number = 50
-    # number_f = random.randint(2, end_number)
-
-    # if sympy.isprime(number_f):
-    #     meaning = 'yes'
-    # else:
-    #     meaning = 'no'
+    if is_prime(number_f):
+        meaning = 'yes'
+    else:
+        meaning = 'no'
 
     return [number_f, meaning]
 
